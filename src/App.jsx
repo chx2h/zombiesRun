95ec7c0998c55a0d11586594031a348fa1c5a883
choihh <january111@gmail.com>
2026-06-17 18:26:23 +0900

    .

--- src/App.jsx
+++ src/App.jsx
@@ -16,15 +16,11 @@ function App() {
   return (
     <div className="App intro-screen" style={{ backgroundImage: `url(${mainImg})` }}> {/* 배경 이미지 적용 */}
       <div className="intro-content">
-        <div className="intro-title-area">
-          <h1 className="intro-title">Zombies Run</h1>
-        </div>
         <div className="intro-warning-message"> {/* 경고 문구 위치 변경 */}
           <p>※ 일반 도로에서 사용 시 횡단보도나 주위 사물을 주의하며 안전하게 이용해 주세요.</p>
         </div>
         <div className="intro-menu">
           <button className="menu-btn" onClick={() => setView('playing')}>START</button>
-          <button className="menu-btn">설정</button>
         </div>
       </div>
     </div>
